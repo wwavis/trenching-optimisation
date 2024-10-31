706e4d4f2fd38736fbc4d6fda7c104c9cc9cb039
wwavis <will24_7@hotmail.co.uk>
2024-10-31 08:57:10 +0000

    docs: minor tweaks

--- optimisation/README.md
+++ optimisation/README.md
@@ -1,4 +1,4 @@
-# ## Run 
+## Run 
 You will need to have run the preprocessing scripts to group the shape files by LOE and saved as GeoJson files
 ```sh
 $ cargo run --release
--- preprocessing/convert_shapefiles_to_geojson/run.sh
+++ preprocessing/convert_shapefiles_to_geojson/run.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p ../../data/raw_geojsons
 
 # Enter filename
-read -p "Enter file path to real_sitedata folder: e.g. (../../../Trenchscrits/real_sitedata)" shape_directory
+read -p "Enter file path to real_sitedata folder, e.g. (../../../Trenchscrits/real_sitedata):" shape_directory
 
 # shape_directory="../../../Trenchscrits/real_sitedata"
 
